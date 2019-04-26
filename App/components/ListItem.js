c0b0f923facdbe9fd89413312a7bc1b1060a2807
Spencer Carli <spencercarli@gmail.com>
2019-04-26 10:24:40 -0500

    Swipeable actiosn

--- App/components/ListItem.js
+++ App/components/ListItem.js
@@ -5,15 +5,18 @@ import {
   StyleSheet,
   Image,
   Platform,
-  TouchableOpacity
+  TouchableOpacity,
+  Animated
 } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
 const styles = StyleSheet.create({
   container: {
     padding: 20,
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "#fff"
   },
   text: {
     fontSize: 18,
@@ -27,27 +30,87 @@ const styles = StyleSheet.create({
   icon: {
     tintColor: "#696969",
     height: 30
+  },
+
+  leftAction: {
+    flex: 1,
+    backgroundColor: "#388e3c",
+    justifyContent: "center"
+  },
+  rightAction: {
+    flex: 1,
+    backgroundColor: "#dd2c00",
+    justifyContent: "center",
+    alignItems: "flex-end"
+  },
+  actionText: {
+    color: "#fff",
+    fontWeight: "600",
+    padding: 20
   }
 });
 
-export const Separator = () => <View style={styles.separator} />;
-
 const isAndroid = Platform.OS === "android";
 
-const ListItem = ({ name, showStar = true, onFavoritePress = () => {} }) => {
+const LeftActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 1],
+    extrapolate: "clamp"
+  });
+  return (
+    <View style={styles.leftAction}>
+      <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
+        Add to Cart
+      </Animated.Text>
+    </View>
+  );
+};
+
+const RightActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [1, 0],
+    extrapolate: "clamp"
+  });
+  return (
+    <View style={styles.rightAction}>
+      <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
+        Delete
+      </Animated.Text>
+    </View>
+  );
+};
+
+export const Separator = () => <View style={styles.separator} />;
+
+const ListItem = ({
+  name,
+  showStar = true,
+  onFavoritePress = () => {},
+  onAddedSwipe,
+  onRemoveSwipe
+}) => {
   const starIcon = isAndroid
     ? require("../assets/icons/md-star-outline.png")
     : require("../assets/icons/ios-star-outline.png");
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{name}</Text>
-      {showStar && (
-        <TouchableOpacity onPress={onFavoritePress}>
-          <Image source={starIcon} style={styles.icon} resizeMode="contain" />
-        </TouchableOpacity>
-      )}
-    </View>
+    <Swipeable
+      renderLeftActions={onAddedSwipe && LeftActions}
+      renderRightActions={onRemoveSwipe && RightActions}
+      onSwipeableLeftOpen={onAddedSwipe}
+      onSwipeableRightOpen={onRemoveSwipe}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>{name}</Text>
+        {showStar && (
+          <TouchableOpacity onPress={onFavoritePress}>
+            <Image source={starIcon} style={styles.icon} resizeMode="contain" />
+          </TouchableOpacity>
+        )}
+      </View>
+    </Swipeable>
   );
 };
 
--- App/screens/CurrentList.js
+++ App/screens/CurrentList.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, View } from "react-native";
+import uuid from "uuid/v4";
 
 import ListItem, { Separator } from "../components/ListItem";
 
 import nachos from "../data/nachos";
 import AddItem from "../components/AddItem";
 
-export default () => (
-  <View style={{ flex: 1 }}>
-    <AddItem />
-    <FlatList
-      data={nachos}
-      keyExtractor={item => item.id}
-      renderItem={({ item }) => <ListItem name={item.name} />}
-      ItemSeparatorComponent={() => <Separator />}
-    />
-  </View>
-);
+const useCurrentList = initialList => {
+  const [list, setList] = useState(initialList);
+
+  const addItem = text => {
+    setList([{ id: uuid(), name: text }, ...list]);
+  };
+
+  const removeItem = id => {
+    const newList = list.filter(item => item.id !== id);
+    setList(newList);
+  };
+
+  return {
+    list,
+    addItem,
+    removeItem
+  };
+};
+
+export default () => {
+  const { list, addItem, removeItem } = useCurrentList(nachos);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <AddItem onSubmitEditing={({ nativeEvent: { text } }) => addItem(text)} />
+      <FlatList
+        data={list}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <ListItem
+            name={item.name}
+            onAddedSwipe={() => removeItem(item.id)}
+            onRemoveSwipe={() => removeItem(item.id)}
+          />
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+    </View>
+  );
+};
